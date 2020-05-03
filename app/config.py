00518004eb5c4ebdb42af7f4c5d039036c2cbae2
Kolesov Daniil <kolesov.daniil39@gmail.com>
2020-05-03 23:31:11 +0300

    change db location

--- app/config.py
+++ app/config.py
@@ -1,11 +1,13 @@
 """"""
 
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config:
+class Config(object):
     """"""
 
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-    RESTPLUS_MASK_SWAGGER = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
