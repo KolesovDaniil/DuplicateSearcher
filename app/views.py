7322e7c6aefbf92adaed880e963dd12d1484030f
Dina Khasanova <dfkhasanova@yandex-team.ru>
2020-05-10 16:56:48 +0300

    finish

--- app/views.py
+++ app/views.py
@@ -8,6 +8,10 @@ import subprocess
 import cv2
 import shutil
 
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
@@ -258,4 +262,47 @@ def get_table_link(mail) -> str:
 
     results = [list(row) for row in query]
 
-    return ''
+    CREDENTIALS_FILE = 'diploma-264613-8c34223b5cf0.json'
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE,
+                                                                   ['https://www.googleapis.com/auth/spreadsheets',
+                                                                    'https://www.googleapis.com/auth/drive'])
+    httpAuth = credentials.authorize(httplib2.Http())
+    service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+
+    spreadsheet = service.spreadsheets().create(body={
+        'properties': {'title': 'Results', 'locale': 'ru_RU'},
+        'sheets': [{'properties': {'sheetType': 'GRID',
+                                   'sheetId': 0,
+                                   'title': '1'}}]
+    }).execute()
+
+    driveService = apiclient.discovery.build('drive', 'v3', http=httpAuth)
+
+    if mail:
+        shareRes = driveService.permissions().create(
+            fileId=spreadsheet['spreadsheetId'],
+            body={'type': 'user', 'role': 'writer', 'emailAddress': mail},
+            fields='id'
+        ).execute()
+
+    else:
+        shareRes = driveService.permissions().create(
+            fileId=spreadsheet['spreadsheetId'],
+            body={'type': 'anyone', 'role': 'writer'},
+            fields='id'
+        ).execute()
+
+    result = [["video1_url", "video1_url", "similarity"]]
+    result.extend(results)
+
+    table = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet['spreadsheetId'], body={
+            "valueInputOption": "USER_ENTERED",
+            "data": [
+                {"range": "1",
+                 "majorDimension": "ROWS",
+                 "values": result}
+            ]
+    }).execute()
+
+    return 'https://docs.google.com/spreadsheets/d/' + spreadsheet['spreadsheetId']
