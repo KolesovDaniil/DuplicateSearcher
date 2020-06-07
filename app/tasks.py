2260a1320f72c4d0bd98e5f1efac2ac15407b13e
Dina Khasanova <dfkhasanova@yandex-team.ru>
2020-06-07 13:01:32 +0300

    some changes

--- app/tasks.py
+++ app/tasks.py
@@ -7,6 +7,8 @@ import os
 import subprocess
 import cv2
 import shutil
+
+from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 from app.database import db
@@ -19,7 +21,21 @@ SUBDIR = MAIN_DIR + 'pict/'
 
 
 @celery_app.task
-def process(id, gauth, service, spreadsheet_id):
+def process(id, service, spreadsheet_id):
+    gauth = GoogleAuth()
+    # Try to load saved client credentials
+    gauth.LoadCredentialsFile("mycreds.txt")
+    if gauth.credentials is None:
+        # Authenticate if they're not there
+        gauth.LocalWebserverAuth()
+    elif gauth.access_token_expired:
+        # Refresh them if expired
+        gauth.Refresh()
+    else:
+        # Initialize the saved creds
+        gauth.Authorize()
+    # Save the current credentials to a file
+    gauth.SaveCredentialsFile("mycreds.txt")
     _list_folder(id, SUBDIR, gauth, service, spreadsheet_id)
 
 
